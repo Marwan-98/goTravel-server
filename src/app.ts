747ef9e9ea4d23de016e82c1623bb5e1992d46eb
Marwan-98 <marwanashrafzaki98@gmail.com>
2022-11-02 10:24:11 +0200

    Update 2

--- src/app.ts
+++ src/app.ts
@@ -6,7 +6,7 @@ import config from "config";
 import logger from "./utils/logget";
 import socket from "./socket";
 
-const port = config.get<number>("port");
+// const port = config.get<number>("port");
 const host = config.get<number>("host");
 const corsOrigin = config.get<string>("corsOrigin");
 
@@ -23,7 +23,7 @@ const io = new Server(httpServer, {
 
 app.get("/", (req, res) => res.sendStatus(200));
 
-httpServer.listen(port, host, () => {
+httpServer.listen(process.env.PORT, host, () => {
   console.log(`Server is listening`);
   socket({ io });
 });
