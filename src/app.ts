5c66206344f60fe20eab7315cb1cd3667ef1f953
Marwan-98 <marwanashrafzaki98@gmail.com>
2022-11-02 10:59:48 +0200

    Socket Update

--- src/app.ts
+++ src/app.ts
@@ -16,7 +16,7 @@ const httpServer = createServer(app);
 
 const io = new Server(httpServer, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "https://gotravel-server.onrender.com",
     credentials: true,
   },
 });
